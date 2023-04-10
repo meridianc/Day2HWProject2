f48db5d149a4eb0d4a0184e6020b6af9fa92f8bc
Office <Office@DESKTOP-8MQLQAH.fios-router.home>
2023-04-10 13:28:42 -0400

    2nd push

--- src/Day2HWDemo2.java
+++ src/Day2HWDemo2.java
@@ -4,7 +4,8 @@ public class Day2HWDemo2 {
 	public static void main(String[] args) {
 		System.out.println("Pushing Day2 HW Project 2....");
 		
+		System.out.println("Pushing second time..");
 	}
-
+	
 	
 }
